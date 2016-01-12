d46ebc25a1be5fe4bc0c6023073ddee469d31613
Sergei Kirjanov <rewlad@gmail.com>
2016-01-12 13:40:08 +0200

    multi html

--- material_ui/src/app.js
+++ material_ui/src/app.js
@@ -7,3 +7,4 @@ const ReactDOM = require('react-dom')
 const RaisedButton = require('material-ui/lib/raised-button')
 
 ReactDOM.render(React.createElement(RaisedButton,{label:"HELLO"}), document.getElementById("app"))
+
--- material_ui/webpack.config.js
+++ material_ui/webpack.config.js
@@ -1,11 +1,23 @@
 
-var config = {
-    entry: "./app.js",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    }
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
+function config(name) {
+    return {
+        entry: "./src/"+name+".js",
+        output: {
+            path: "build",
+            filename: name + ".js"
+        },
+        plugins: [new HtmlWebpackPlugin({
+            filename: name + ".html",
+            title: name,
+            hash: true,
+            favicon: "./src/favicon.png"
+        })]
+    }
 }
 
-module.exports = config
+module.exports = [
+    config("hello"),
+    config("app")
+]
