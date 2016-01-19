f0d624d8d61ae2073a201ea03b1c8d145e3b2d75
Sergei Kirjanov <rewlad@gmail.com>
2016-01-19 12:04:42 +0200

    starting with react-grid-layout

--- client/src/test/btn.js
+++ client/src/test/btn.js
@@ -2,23 +2,34 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-/*
-const React = require('react')
-const ReactDOM = require('react-dom')
-const RaisedButton = require('material-ui/lib/raised-button')
-*/
-
-/*
-import RaisedButton from 'material-ui/lib/raised-button'
-ReactDOM.render(React.createElement(RaisedButton,{label:"HELLO"}), document.getElementById("app"))
-*/
-
 import ui from "material-ui/lib"
+import ReactGridLayout from "react-grid-layout"
+
+const m = React.createElement
+
+
+const Login = () => m(
+    ReactGridLayout,
+    {
+        key:1,
+        cols: 3,
+        className: "layout",
+        //rowHeight: 300,
+        verticalCompact: false,
+        layout: [{i:1,x:1,y:2,w:1,h:1}]
+    },[
+        m(ui.Paper,{key:1, margin: 20},[
+            m(ui.RaisedButton,{label:"LOGIN", key:1 })
+        ])
+    ]
+)
+
+const viewByHash = ({Login})
+
 
 const appElement = document.createElement("div")
 document.body.appendChild(appElement)
+ReactDOM.render(viewByHash[location.hash.substring(1)](), appElement)
 
-ReactDOM.render(React.createElement(ui.RaisedButton,{label:"HELLO"}), appElement)
 
 
