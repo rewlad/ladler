3bcff3253cdc7b7429082423b1d240a902bb46e0
Sergei Kirjanov <rewlad@gmail.com>
2016-04-27 12:26:51 +0300

    JsonBuilder fix

---
+++ server/src/test/scala/ee/cone/base/vdom/JSONTest.scala
@@ -0,0 +1,21 @@
+package ee.cone.base.vdom
+
+object JSONTest extends App {
+  val builder = new JsonBuilderImpl()
+
+  def make(left: Int): Unit = {
+    if(left>0) {
+      builder.append(left.toString)
+      builder.append(left.toString)
+      builder.append(left.toString)
+      builder.startObject()
+      make(left - 1)
+      builder.end()
+      builder.append(left.toString)
+      builder.append(left.toString)
+    }
+  }
+
+  make(63)
+  println(builder.result.toString)
+}
