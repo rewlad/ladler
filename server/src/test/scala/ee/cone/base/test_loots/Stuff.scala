78b6fc347341f850e7926ac8878162362fba0cce
Sergei Kirjanov <rewlad@gmail.com>
2016-05-23 15:48:19 +0300

    draft invalidate, login

--- server/src/test/scala/ee/cone/base/test_loots/Stuff.scala
+++ server/src/test/scala/ee/cone/base/test_loots/Stuff.scala
@@ -135,7 +135,7 @@ class DataTablesState(currentVDom: CurrentVDom){
     def value = widthOfTables.getOrElse(id,0.0f)
     def value_=(value: Float) = {
       widthOfTables(id) = value
-      currentVDom.invalidate() // todo: replace by throttled
+      currentVDom.until(System.currentTimeMillis+200)
     }
   }
   private val rowToggledOfTables = collection.mutable.Map[VDomKey,VDomKey]()
@@ -363,6 +363,7 @@ class TestComponent(
 
   private def wrapDBView[R](view: ()=>R): R =
     eventSource.incrementalApplyAndView { () ⇒
+      if(!loggedIn){ return loginView() }
       val startTime = System.currentTimeMillis
       val res = view()
       val endTime = System.currentTimeMillis
@@ -372,6 +373,11 @@ class TestComponent(
 
   private def paperWithMargin(key: VDomKey, child: ChildPair[OfDiv]) =
     withMargin(key, 10, paper("paper", withPadding(key, 10, child)))
+
+  def loggedIn = true //todo
+  private def loginView() = {
+
+  }
 
   private def entryListView(pf: String) = wrapDBView{ ()=>{
     val filterObj = filters.filterObj("/entryList")
@@ -732,10 +738,7 @@ class TestComponent(
     ))
   }
 
-  private def saveAction()() = {
-    eventSource.addRequest()
-    currentVDom.invalidate()
-  }
+  private def saveAction()() = eventSource.addRequest()
 
   private def toolbar(title:String): ChildPair[OfDiv] = {
     paperWithMargin("toolbar", divWrapper("toolbar",None,Some("200px"),None,None,None,None,
@@ -798,6 +801,7 @@ class TestComponent(
     onUpdate.handlers(List(logAt.asWork,logAt.workStart,logAt.workStop).map(attrFactory.attrId(_)), calcWorkDuration) :::
     onUpdate.handlers(List(logAt.asWork,logAt.workDuration,logAt.entryOfWork).map(attrFactory.attrId(_)), calcEntryDuration) :::
     onUpdate.handlers(List(logAt.asEntry,logAt.confirmedOn).map(attrFactory.attrId(_)), setEntryConfirmDate) :::
+    CoHandler(SessionInstantProbablyAdded)(currentVDom.invalidate) ::
     Nil
 }
 
